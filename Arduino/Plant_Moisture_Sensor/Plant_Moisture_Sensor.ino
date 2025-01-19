353770055eef631e630be43d8b1c014897320bfe
MajortomVR <majortom.rb@gmail.com>
2025-01-19 20:48:33 +0100

    feat: Add a status LED and periodic measurements

--- Arduino/Plant_Moisture_Sensor/Plant_Moisture_Sensor.ino
+++ Arduino/Plant_Moisture_Sensor/Plant_Moisture_Sensor.ino
@@ -2,10 +2,16 @@
   I measured 289 when put in a glass of water
   642 room air moisture
 */
+const unsigned long PERIODIC_MEASUREMENT_INTERVAL_MILLISECONDS = 5UL * 60UL * 1000UL; // every 5 minutes
 
-int SENSOR_POWER_PIN = 2;           // VCC for Sensor
-int SENSOR_ADC_PIN = A0;
+const int LED_PIN = 3;
+const int SENSOR_POWER_PIN = 2;           // VCC for Sensor
+const int SENSOR_ADC_PIN = A0;
 bool serialConnectionOpen = false;
+
+int lastSensorMeasurementValue = 0;
+unsigned long lastPeriodicMeasurementTimestamp = 0;
+unsigned long lastLedFlashTimestamp = 0;
 
 /**
   Initial Setup.
@@ -13,6 +19,7 @@ bool serialConnectionOpen = false;
 void setup() {
   Serial.begin(9600);
   pinMode(SENSOR_POWER_PIN, OUTPUT);
+  pinMode(LED_PIN, OUTPUT);
   // Deactivate Sensor
   digitalWrite(SENSOR_POWER_PIN, LOW);
 }
@@ -33,10 +40,34 @@ void handleConnectionClosed() {
 }
 
 /**
+  Sensor reading.
+*/
+int readSensor() {
+  digitalWrite(LED_PIN, HIGH);
+  // Activate Sensor
+  digitalWrite(SENSOR_POWER_PIN, HIGH);
+  delay(500);
+
+  const int measurementCount = 5;
+  int total = 0;
+
+  for (int i=0; i<measurementCount; i++) {
+    total += analogRead(SENSOR_ADC_PIN);
+    delay(5);
+  }
+  
+  digitalWrite(LED_PIN, LOW);
+  return total / measurementCount;  
+}
+
+/**
   MAIN LOOP
 */
 void loop() {  
-  delay(200);
+  // Deactivate Sensor (To reduce power consumption it is only activated when needed)
+  digitalWrite(SENSOR_POWER_PIN, LOW);
+  delay(100);
+
   // When a client connects, send an identifier back. This is used to find all the connected plant sensors by the client program.
   if (Serial && !serialConnectionOpen) {
     handleConnectionOpened();
@@ -45,11 +76,8 @@ void loop() {
     handleConnectionClosed();
   }
 
-  // Sensor
-  if (Serial && serialConnectionOpen) {
-    // Deactivate Sensor (To reduce power consumption it is only activated when needed)
-    digitalWrite(SENSOR_POWER_PIN, LOW);  
-    delay(100);
+  // Connection Management
+  if (Serial && serialConnectionOpen) {    
     bool start_measurement = false;
 
     // Serial communication message handling
@@ -57,24 +85,25 @@ void loop() {
       char incomingByte = Serial.read();
       start_measurement = true;
     }
-
-    // If we have a start_measurement signal -> Read the sensor value from the ADC and send it on the serial connection.
+    
     if (start_measurement) {
-      Serial.println("Measuring...");    
-      // Activate Sensor
-      digitalWrite(SENSOR_POWER_PIN, HIGH);
-      delay(1000);
-
-      const int measurementCount = 5;
-      int value = 0;
-
-      for (int i=0; i<measurementCount; i++) {
-        value += analogRead(SENSOR_ADC_PIN);
-        delay(5);
-      }
-
-      value /= measurementCount;
-      Serial.println(value);
+      Serial.println("Measuring...");
+      lastSensorMeasurementValue = readSensor();
+      Serial.println(lastSensorMeasurementValue);
     }
   }
+
+  // Periodic measurements
+  if (millis() - lastPeriodicMeasurementTimestamp >= PERIODIC_MEASUREMENT_INTERVAL_MILLISECONDS) {
+    lastPeriodicMeasurementTimestamp = millis();
+    lastSensorMeasurementValue = readSensor();
+  }
+
+  // TODO: Get max value from EEPROM
+  if (lastSensorMeasurementValue > 500 && millis() - lastLedFlashTimestamp >= 3000) {
+    lastLedFlashTimestamp = millis();
+    digitalWrite(LED_PIN, HIGH);
+    delay(100);
+    digitalWrite(LED_PIN, LOW);
+  }  
 }
